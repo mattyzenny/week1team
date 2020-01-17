5a808608c09d18b0142dc49a8140353683bafc31
Matthew Zendejas <user@matthewzendejas@hotmail.com>
2020-01-17 16:09:15 -0500

    trial

--- java/src/main/java/com/techelevator/MakeChange.java
+++ java/src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,5 @@
 package com.techelevator;
-
+import java.util.Scanner;
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
  display the change required.
@@ -12,7 +12,37 @@ package com.techelevator;
 public class MakeChange {
 
 	public static void main(String[] args) {
+				
+				try (Scanner in = new Scanner(System.in)){
+					
+					boolean change = false;
+					
+					while (!change) {
+						System.out.println("Your total amounts to: 23.65");
+						System.out.println("Please enter your bill");
+						String answer = in.nextLine();
+						
+						
+						 if (answer.equals("100.00") || (answer.equals("100"))) {
+							 change = true;
+						System.out.println("Your change is 76.35");
+					}
+						 
+						 else {
+								System.out.println("Please insert appropriate amount");
 
-	}
+					}
+				
+				}
+			
 
-}
+				}
+			
+			}
+		}
+					
+	
+			
+			
+	
+	
--- java/src/main/java/com/techelevator/MartianWeight.java
+++ java/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,5 @@
 package com.techelevator;
-
+import java.util.Scanner;
 /*
  In case you've ever pondered how much you weight on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -11,6 +11,7 @@ and displays each Earth weight as itself, and its Martian equivalent.
  $ MartianWeight 
  
 Enter a series of Earth weights (space-separated): 98 235 185
+//SPLIT COMMAND// //EACH ONE MUST BE PARSED INTO NUMERIC VALUE AND THEN OUT//
  
  98 lbs. on Earth, is 37 lbs. on Mars.
  235 lbs. on Earth, is 88 lbs. on Mars.
@@ -19,7 +20,40 @@ Enter a series of Earth weights (space-separated): 98 235 185
 public class MartianWeight {
 
 	public static void main(String[] args) {
+ try (Scanner scanner = new Scanner(System.in)) {
+		 
+	boolean problemSolve = true;
+		while(problemSolve) {
+			
+			System.out.println("Enter your weight: ");
+			String value = scanner.nextLine();
+			
+			 int weightToConvert = Integer.parseInt(value);
+			
+			System.out.println("Enter (e) for weight on earth, (m) for weight on mars: ");
 
+			String placeOfConversion = scanner.nextLine();
+			
+			if (placeOfConversion.equalsIgnoreCase("e")) {
+				float weight = weightToConvert * .378f;
+				System.out.println("your weight is: " + ((int)weight));
+			}
+			else if (placeOfConversion.equalsIgnoreCase("m")) {
+				float weight = weightToConvert /.378f;
+				System.out.println("your weight is: " + ((int)weight));
+			}
+			else {
+				System.out.println("Try AGAIN!!");
+			}
+			
+		}
+		
+		
+ }
+		
 	}
 
 }
+
+
+//while on mars.....if mars weight = X, earth weight = Y//
